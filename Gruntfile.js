ffa182e583f1f696fcbf016c9da65e1c69b7ecf0
viskin <r.viskin@terikon.com>
2015-08-01 19:11:27 +0300

    Updated company name in copyright

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,7 +14,7 @@ module.exports = function(grunt) {
 		uglify: {
 			options: {
 				sourceMap: true,
-				banner: '/* <%= grunt.task.current.target %> v<%= pkg.version %> <%= grunt.template.today("dd-mm-yyyy") %> (C) 2015 Terikon Software */\n'
+				banner: '/* <%= grunt.task.current.target %> v<%= pkg.version %> <%= grunt.template.today("dd-mm-yyyy") %> (C) 2015 Terikon Apps */\n'
 			},
 			'kendo-pouchdb': {
 				src: 'kendo-pouchdb.js',
